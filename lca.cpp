858788385bd6f3087a76c52469959bdecda3ea34
Kunal Kashyap <67000702+Kunal1522@users.noreply.github.com>
2025-06-04 20:21:25 +0530

    Update lca.cpp

--- lca.cpp
+++ lca.cpp
@@ -1,88 +1,68 @@
-// Taken from striver_79's github repo 
-// Stores the parent of every node 
-int par[N][21];
-
-// Stores the dept 
-int dep[N];
-
-// Stores the tree 
-vector<int> g[N];
-
-// Function to pre-compute DFS
-void dfs(int v, int p = 0)
+const int N = 200005;
+vector<int> tree[N], level(N);
+int up[21][N];
+void dfs(int src, int par, int lev)
 {
-
-    // First parent
-    par[v][0] = p;
-
-    // Finds the parent at 2^i
-    for (int i = 1; i <= 20; i++)
-        par[v][i] = par[par[v][i - 1]][i - 1];
-
-    // Call DFS function
-    for (int i = 0; i < g[v].size(); i++) {
-        int to = g[v][i];
-
-        // Already visited
-        if (to == p)
-            continue;
-        
-        // Find the depth 
-        dep[to] = dep[v] + 1;
-        
-        // Call the Dfs Function 
-        dfs(to, v);
+    level[src] = lev;
+    up[0][src] = par;
+    for (auto ch : tree[src])
+    {
+        if (ch != par)
+            dfs(ch, src, lev + 1);
     }
 }
-
-
-
-
-// Function to find the lca of a and b 
-int lca(int a, int b)
+void preprocess(int n)
 {
-
-    // If a is higher than b then swap them 
-    // Tree me a niche rahegi and b upar hamesha 
-    if (dep[a] < dep[b])
-        swap(a, b);
-
-    // Make both of them at equal height 
+    dfs(1, -1, 0);
+    for (int h = 1; (1 << h) <= n; h++)
+    {
+        for (int i = 1; i <= n; i++)
+            up[h][i] = up[h - 1][up[h - 1][i]];
+    }
+}
+int LCA(int x, int y)
+{
+    // calc diff -> lift diff ->if same then return else lift both
+ 
+    if (level[x] < level[y])
+        swap(x, y);
+    int diff = level[x] - level[y];
+    for (int i = 20; i >= 0; i--)
+        if (diff & (1 << i))
+            x = up[i][x];
+    if (x == y)
+        return y;
+    // lift both
     for (int i = 20; i >= 0; i--)
     {
-        if (dep[a] - (1 << i) >= dep[b])
-            a = par[a][i];
-    }
-
-    // Now traverse till up 
-    // till we donot get parent same 
-    for (int i = 20; i >= 0; --i)
-    {
-        if (par[a][i] != par[b][i])
+        if (up[i][x] != up[i][y])
         {
-            a = par[a][i],
-            b = par[b][i];
+            x = up[i][x];
+            y = up[i][y];
         }
     }
-    
-    // Till now not same 
-    if (a != b)
-        a = par[a][0];
-
-    return a;
+    return up[0][x];
 }
-
-// Function to find the distance between a and b 
-int solve(int a, int b)
+void solve()
 {
-    if (a == b)
-        return 0;
-
-    if (dep[a] < dep[b])
-        swap(a, b);
-
-    // Find lca
-    int q = lca(a, b);
-    int dist = dep[a] + dep[b] - 2 * dep[q];
-    return dist;
+    int n;
+    cin >> n;
+    int q;
+    cin >> q;
+    for (int i = 1; i <= n - 1; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        tree[a].push_back(b);
+        tree[b].push_back(a);
+    }
+    preprocess(n);
+    while (q--)
+    {
+        int a, b;
+        cin >> a >> b;
+        int lca = LCA(a, b);
+        int k = min(level[a], level[b]);
+        cout << abs(level[a] - level[b]) + 2 * abs(level[lca] - k) << endl;//this finds the distance between two nodes
+    }
 }
